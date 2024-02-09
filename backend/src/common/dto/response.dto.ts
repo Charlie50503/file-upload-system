03c69f68bbd816634cf3cf08547576e14f9b4968
charlie50503 <cate50503@gmail.com>
2024-02-09 15:21:10 +0800

    feat: [API] Interface→添加共用 response dto

---
+++ backend/src/common/dto/response.dto.ts
@@ -0,0 +1,28 @@
+// common/dto/response.dto.ts
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ResponseDto<T> {
+  @ApiProperty({
+    description: '狀態碼',
+    example: 200,
+  })
+  statusCode: number;
+  @ApiProperty({
+    description: '訊息',
+    example: 'success',
+  })
+  message: string;
+  @ApiProperty({
+    description: '資料',
+    example: 'data',
+    type: 'object',
+    isArray: true,
+  })
+  data?: T;
+  @ApiProperty({
+    description: '錯誤訊息',
+    example: 'error',
+  })
+  error?: string;
+}

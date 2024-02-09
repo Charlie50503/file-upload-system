f657441a2de2cd8e13fa3bfe6a691e636ceac4f0
charlie50503 <cate50503@gmail.com>
2024-02-09 15:12:53 +0800

    feat: [UI] 登入頁/共用組件/confirm dialog→添加 確認 dialog 組件

---
+++ frontend/src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Subject } from 'rxjs';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
+export interface ConfirmDialogInputData {
+  message: string;
+}
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+})
+export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogInputData,
+  ) {}
+
+  submit() {
+    this.dialogRef.close(true);
+  }
+}

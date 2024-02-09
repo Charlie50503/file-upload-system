4c9b239d1f2339a7494a1950343f1d53bbd6bd07
charlie50503 <cate50503@gmail.com>
2024-02-09 15:23:44 +0800

    feat: [API] app.module→更新功能

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { FileManagementController } from './controllers/file-management/file-management.controller';
+import { FileManagementModule } from './modules/file-management/file-management.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env`],
+      cache: true,
+      isGlobal: true,
     }),
+    FileManagementModule,
   ],
-  controllers: [AppController, FileManagementController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}

716bfb2b315e61b609b0e658a707c2ddd85d9972
charlie50503 <cate50503@gmail.com>
2024-02-10 19:39:21 +0800

    feat: [UI] file-management→更新 功能
    1. 在各個 request 補上 共用 error handler 請求
    2. 添加 sort 功能
    3. table 上顯示時間改成可視化
    4. 完善上傳功能

--- frontend/src/app/pages/file-management/file-management.component.ts
+++ frontend/src/app/pages/file-management/file-management.component.ts
@@ -1,13 +1,15 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { filter, map, switchMap } from 'rxjs';
 import { IFile } from 'src/app/api/v1/models';
 import { FileManagementService } from 'src/app/api/v1/services';
 import { FileSizePipe } from 'src/app/core/pipes/file-size.pipe';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 
 @Component({
@@ -21,65 +23,76 @@ import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-di
     MatIconModule,
     FileSizePipe,
     AsyncPipe,
+    MatSortModule,
+    DatePipe,
   ],
 })
-export class FileManagementComponent implements OnInit {
-  displayedColumns: string[] = [
+export class FileManagementComponent implements AfterViewInit {
+  @ViewChild(MatSort) sort!: MatSort;
+  public displayedColumns: string[] = [
     'name',
     'size',
     'updatedDate',
     'createdDate',
     'active',
   ];
-  dataSource: IFile[] = [];
+  public dataSource = new MatTableDataSource<IFile>([]);
 
   constructor(
     private fileManagementService: FileManagementService,
     public dialog: MatDialog,
+    private errorHandlerService: ErrorHandlerService,
   ) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
     this.fetchData();
   }
 
-  fetchData() {
+  public fetchData() {
     this.fileManagementService
       .fileManagementControllerGetFiles()
       .pipe(map((res) => res.data as IFile[]))
-      .subscribe((data) => {
-        this.dataSource = data;
+      .subscribe({
+        next: (data) => {
+          this.dataSource.data = data;
+        },
+        error: this.errorHandlerService.handleRequestError.bind(this),
       });
   }
 
-  downloadFile(element: IFile) {
+  public downloadFile(element: IFile) {
     this.fileManagementService
       .fileManagementControllerDownload$Response({
         fileName: element.name,
       })
-      .subscribe((res) => {
-        const contentType = res.headers.get('content-type')!;
-        const bold = res.body as unknown as Blob;
-        var blob = new Blob([bold], { type: contentType });
-        const downloadUrl = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = downloadUrl;
-        console.log(downloadUrl);
+      .subscribe({
+        next: (res) => {
+          const contentType = res.headers.get('content-type')!;
+          const bold = res.body as unknown as Blob;
+          var blob = new Blob([bold], { type: contentType });
+          const downloadUrl = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = downloadUrl;
+          console.log(downloadUrl);
 
-        a.download = element.name;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(downloadUrl);
+          a.download = element.name;
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(downloadUrl);
+        },
+        error: this.errorHandlerService.handleRequestError.bind(this),
       });
   }
 
-  deleteFile(element: IFile) {
+  public deleteFile(element: IFile) {
     return this.fileManagementService.fileManagementControllerDelete({
       fileName: element.name,
     });
   }
 
-  openDeleteConfirmDialog(element: IFile): void {
+  public openDeleteConfirmDialog(element: IFile): void {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       data: { message: '確定刪除嗎?' },
       panelClass: ['w-[240px]'],
@@ -91,8 +104,42 @@ export class FileManagementComponent implements OnInit {
         filter((res) => !!res),
         switchMap(() => this.deleteFile(element)),
       )
-      .subscribe((data) => {
-        this.fetchData();
+      .subscribe({
+        next: (data) => {
+          this.fetchData();
+        },
+        error: this.errorHandlerService.handleRequestError.bind(this),
+      });
+  }
+
+  public fileChanged(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const blob: Blob = new Blob([reader.result as ArrayBuffer], {
+          type: file.type,
+        });
+        this.callApiWithBlob(blob, file.name);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
+
+  private callApiWithBlob(blob: Blob, fileName: string) {
+    this.fileManagementService
+      .fileManagementControllerUpload$Response({
+        body: {
+          file: blob,
+          fileName,
+        },
+      })
+      .subscribe({
+        next: (data) => {
+          this.fetchData();
+        },
+        error: this.errorHandlerService.handleRequestError.bind(this),
       });
   }
 }

ec1c87218eecf6970be919dbde442706ba94b7c2
charlie50503 <cate50503@gmail.com>
2024-02-10 19:32:16 +0800

    feat: [API] exception→multer exception 改回傳客製化錯誤訊息

--- backend/src/common/filters/multer-exception.filter.ts
+++ backend/src/common/filters/multer-exception.filter.ts
@@ -5,6 +5,7 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { ErrorCode } from '../enum/error-code.enum';
 
 @Catch()
 export class MulterExceptionFilter implements ExceptionFilter {
@@ -16,17 +17,15 @@ export class MulterExceptionFilter implements ExceptionFilter {
     if (exception.message === 'File too large') {
       response.status(HttpStatus.BAD_REQUEST).json({
         statusCode: HttpStatus.BAD_REQUEST,
-        timestamp: new Date().toISOString(),
-        path: ctx.getRequest().url,
-        message: '檔案不可以超過 10MB',
+        errorMessage: '檔案不可以超過 10MB',
+        errorCode: ErrorCode.FILE_OVERSIZE,
       });
     } else {
       // 对于不是由 Multer 引起的其他异常，可以根据需要进行处理
       response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        timestamp: new Date().toISOString(),
-        path: ctx.getRequest().url,
-        message: 'Internal Server Error',
+        errorMessage: 'Internal Server Error',
+        errorCode: ErrorCode.INTERNAL_SERVER_ERROR,
       });
     }
   }

c54443d665bcf02c62528cde8bd5b883e6080e2f
charlie50503 <cate50503@gmail.com>
2024-02-10 15:24:29 +0800

    feat: [API] validator/createUserDto→添加自定義錯誤訊息

--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,29 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  minLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: '請輸入用戶名' })
+  @IsNotEmpty({ message: '請輸入用戶名' })
   @ApiProperty({
-    description: '帳號',
+    description: '用戶名',
     example: 'username',
     type: 'string',
   })
-  @IsString()
-  @IsNotEmpty()
   name: string;
+  @IsString({ message: '請輸入密碼' })
+  @IsNotEmpty({ message: '請輸入密碼' })
+  @MinLength(6, { message: '密碼長度至少為 6' })
   @ApiProperty({
     description: '密碼',
     example: 'password',
     type: 'string',
   })
-  @IsString()
-  @IsNotEmpty()
   password: string;
   @ApiProperty({
     description: 'email',
     example: 'email',
     type: 'string',
   })
+  @IsString({ message: '請輸入email' })
+  @IsNotEmpty({ message: '請輸入email' })
+  @IsEmail({}, { message: '請輸入正確的email格式' })
   email: string;
 }

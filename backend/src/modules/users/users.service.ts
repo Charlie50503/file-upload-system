75b98a9a51ce36fa1af0777d6def8bf80b0a8ad4
古家寧 <cate50503@gmail.com>
2024-04-24 11:03:25 +0800

    feat [API]: User/Service→刪除未使用 import

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel, getConnectionToken } from '@nestjs/mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';

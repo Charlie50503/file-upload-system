f8ed076b6c3f45e4d871a03aa6c5a7282b05f227
古家寧 <cate50503@gmail.com>
2024-04-24 16:42:04 +0800

    feat [API]: SessionNoteService→補上 isDataExist 行為

--- backend/src/modules/muscle-tag/muscle-tag.service.ts
+++ backend/src/modules/muscle-tag/muscle-tag.service.ts
@@ -52,4 +52,9 @@ export class MuscleTagService {
     const foundItem = await this.muscleTagModel.findById(id);
     return foundItem;
   }
+
+  public async isDataExist(id: string): Promise<boolean> {
+    // 確認資料是否存在 使用 != 比對 null 跟 undefined
+    return (await this.muscleTagModel.exists({ _id: id })) != null;
+  }
 }

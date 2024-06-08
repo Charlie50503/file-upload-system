764a9a143de094bd78bdf2fb1d928c34832eac9e
charlie50503 <cate50503@gmail.com>
2024-06-08 13:18:44 +0800

    style: 刪除未使用 import

--- frontend/src/app/core/interceptors/spinner.interceptor.ts
+++ frontend/src/app/core/interceptors/spinner.interceptor.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 import { PrimarySpinnerService } from 'src/app/commons/shared/spinner/primary-spinner/primary-spinner.service';
-import { SecondarySpinnerService } from 'src/app/commons/shared/spinner/secondary-spinner/secondary-spinner.service';
 
 export const spinnerInterceptor: HttpInterceptorFn = (req, next) => {
   const primarySpinnerService = inject(PrimarySpinnerService);
